b70519b7af1edab4b7113061b69a2b56c09c46ae
Lucas Terra <lucas.terra8587@gmail.com>
2025-02-06 13:00:24 -0300

    player 2 comments

--- Player2/player_2.gd
+++ Player2/player_2.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED := 800.0
 
+# Codigo quase identico ao script de 'player.gd'
+# A unica diferenca seriam os inputs 'down2' -> setinha pra baixo
+# e 'up2' -> setinha pra cima
+
 func getYDir() -> float:
 	return Input.get_action_strength("down2") - Input.get_action_strength("up2")
 
